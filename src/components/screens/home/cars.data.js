2ffc12f5530b0853662aff6547759803bfdc372f
turelyk-arsen <114808096+turelyk-arsen@users.noreply.github.com>
2023-06-17 12:10:34 +0200

    fix foto

--- src/components/screens/home/cars.data.js
+++ src/components/screens/home/cars.data.js
@@ -3,18 +3,18 @@ export const cars = [
         id: 1,
         name: 'Toyota Camry',
         price: 24970,
-        image: '/public/1.jpg'
+        image: './1.jpg'
     },
     {
         id: 2,
         name: 'Honda Civic',
         price: 29970,
-        image: '/public/2.jpg'
+        image: './2.jpg'
     },
     {
         id: 3,
         name: 'Honda Civic 2',
         price: 39970,
-        image: '/public/3.jpg'
+        image: './3.jpg'
     },
 ]

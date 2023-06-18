5a6c5ef3ec9a13eb73ac962603cac6bc1bb652be
turelyk-arsen <114808096+turelyk-arsen@users.noreply.github.com>
2023-06-18 22:10:04 +0200

    login

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,9 +1,10 @@
 // import styles from "./Home.module.css";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import CarItem from "./car-item/CarItem";
 // import { cars as carsData } from "./cars.data";
 import CreateCarForm from "./create-car-form/CreateCarForm";
 import { CarService } from "../../../services/car.service";
+import { AuthContext } from "../../../providers/AuthProvider";
 
 const Home = () => {
   // const filteredCars = useMemo(()=> cars.filter(car => car.price > 20000), [])
@@ -17,9 +18,21 @@ const Home = () => {
     fetchData();
   }, []);
 
+const {user, setUser} = useContext(AuthContext);
+
   return (
     <div>
       <h1>Cars catalog</h1>
+
+      {user ? (
+        <>
+        <h2>Welcome, {user.name}!</h2>
+        <button className='btn' onClick={() =>setUser(null)}>Logout</button>
+        </>
+      ) : (
+        <button className='btn' onClick={() =>setUser({name: 'Max',})}>Login</button>
+      )}
+
       <CreateCarForm setCars={setCars} />
       {cars.length ? (
         cars.map((car) => <CarItem key={car.id} car={car} />)
